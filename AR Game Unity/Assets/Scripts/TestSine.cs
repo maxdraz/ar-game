5f30e895bcab7fee94607318a33d64ec60cc74b9
Max <38158634+maxdraz@users.noreply.github.com>
2019-04-01 21:11:10 +0100

    Added image database and messing around

---
+++ AR Game Unity/Assets/Scripts/TestSine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSine : MonoBehaviour
+{
+    float period;
+    float frequency;
+    float angularFrequency;
+    float elapsedTime;
+    float amplitude;
+    float phase;
+
+    void SmoothSineWave()
+    {
+        // y(t) = A * sin(ωt + θ) [Basic Sine Wave Equation]
+        // [A = amplitude | ω = AngularFrequency ((2*PI)f) | f = 1/T | T = [period (s)] | θ = phase | t = elapsedTime]
+        // Public/Serialized Variables: amplitude, period, phase
+        // Private/Non-serialized Variables: frequency, angularFrequency, elapsedTime
+        // Local Variables: omegaProduct, y
+
+        // If the value of period has altered last known frequency...
+        if (1 / (period) != frequency)
+        {
+            // Recalculate frequency & omega.
+            frequency = 1 / (period);
+            angularFrequency = (2 * Mathf.PI) * frequency;
+        }
+        // Update elapsed time.
+        elapsedTime += Time.deltaTime;
+        // Calculate new omega-time product.
+        float omegaProduct = (angularFrequency * elapsedTime);
+        // Plug in all calculated variables into the complete Sine wave equation.
+        float y = (amplitude * Mathf.Sin(omegaProduct + phase));
+        // 
+        transform.localPosition = new Vector3(0, y, 0);
+    }
+}
---
+++ AR Game Unity/Assets/Scripts/Turret.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    public List<Turret> targets;
+    public Transform target;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float magnitude;
+    [SerializeField] private float theta;
+
+
+    private void Start()
+    {
+        foreach (Turret t in GameObject.FindObjectsOfType<Turret>())
+        {
+            if (t != null)
+            {
+                if (t != this)
+                {
+                    targets.Add(t);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theta += Time.deltaTime;
+
+        if(target != null)
+        {
+            ShootTarget(target);
+        }
+        else
+        {
+            ChooseTarget();
+        }
+        //sinewave        
+        //Levitate();
+     
+        
+    }
+
+    void ChooseTarget()
+    {
+        if (targets.Count > 0)
+        {
+            target = targets[Random.Range(0, targets.Count - 1)].transform;
+        }
+    }
+
+    void ShootTarget(Transform target)
+    {
+        transform.LookAt(target);
+    }
+
+    void Levitate()
+    {
+        Vector3 pos = transform.position;
+        float newY = Mathf.Sin(Time.time * speed);
+        
+        transform.position = new Vector3(pos.x, newY, pos.z) * magnitude;
+       
+              
+    }
+}
