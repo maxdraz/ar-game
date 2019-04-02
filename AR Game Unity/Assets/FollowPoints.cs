1d7686914a540ddfaacfd7d1abb783a8f5bf387f
ZabaMan <47353137+ZabaMan@users.noreply.github.com>
2019-04-02 14:30:18 +0100

    Added game manager, follow path and UI

---
+++ AR Game Unity/Assets/FollowPoints.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPoints : MonoBehaviour
+{
+    [SerializeField] private Path path;
+    public int currentPoint;
+    [SerializeField] private float speed;
+    public Transform pointer;
+
+    private void Start()
+    {
+        
+        pointer.position = path.transform.GetChild(0).position;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.LookAt(pointer, pointer.up);
+
+        if (Vector3.Distance(transform.position, pointer.position) < 0.1f)
+        {
+            currentPoint++;
+            if (currentPoint == path.pathPoints.Count)
+            {
+                currentPoint = 0;
+            }
+            pointer.position = path.pathPoints[currentPoint].position;
+        }
+    }
+}
---
+++ AR Game Unity/Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    private int ObjectsToChange;
+    [SerializeField] private TextMeshProUGUI objectsLeft;
+
+    public void AddToObjectToChange()
+    {
+        ObjectsToChange++;
+        objectsLeft.text = ObjectsToChange.ToString();
+    }
+
+    public void SubtractToObjectToChange()
+    {
+        ObjectsToChange--;
+        objectsLeft.text = ObjectsToChange.ToString();
+    }
+
+
+}
