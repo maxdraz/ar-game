0c0387456792c3348f7db5470a6a730239ad0d41
Max <38158634+maxdraz@users.noreply.github.com>
2019-04-03 18:07:33 +0100

    Added points text

---
+++ AR Game Unity/Assets/Scripts/LookAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform camera;
+
+    private void Awake()
+    {
+        if (GameObject.FindWithTag("MainCamera") != null)
+        {
+            camera = GameObject.FindWithTag("MainCamera").transform;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(camera);
+    }
+}
