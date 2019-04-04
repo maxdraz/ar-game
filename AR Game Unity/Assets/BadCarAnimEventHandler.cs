ddbbbaf12543b63ac01a802077633907bb017311
NicolasStevenson <38294505+NicolasStevenson@users.noreply.github.com>
2019-04-04 17:58:11 +0100

    Adding BadCar Anim and Audio

---
+++ AR Game Unity/Assets/BadCarAnimEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadCarAnimEventHandler : MonoBehaviour
+{
+    public AudioSource audio;
+
+   
+
+  
+    public void PlaySound() {
+        audio.Play();
+    }
+}
